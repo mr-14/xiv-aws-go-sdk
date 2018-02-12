5a6104850ac8e57bbc7d46605afe960154c2eda1
zj <suke.shou@gmail.com>
2018-02-12 20:10:29 +0800

    Add authorizer

---
+++ httputil/authorizer.go
@@ -0,0 +1,30 @@
+package httputil
+
+import (
+	"fmt"
+	"net/http"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/mr-14/xiv-aws-go-sdk/errorutil"
+)
+
+// ValidateJWT validates Json web token
+func ValidateJWT(tokenString string, tokenKey string) (map[string]interface{}, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(tokenKey), nil
+	})
+	if err == nil {
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			return claims, nil
+		}
+	}
+
+	return nil, &errorutil.HTTPError{
+		Status: http.StatusUnauthorized,
+		Form:   &errorutil.FormError{Message: "error.jwt.invalid"},
+	}
+}
--- httputil/request.go
+++ httputil/request.go
@@ -5,34 +5,35 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
-// Context defines service container
+// Context defines service ctx
 type Context struct {
+	Params   map[string]string
 	DynamoDB *dynamodb.DynamoDB
 }
 
 // HandlerSpec defines request handlers
 type HandlerSpec struct {
-	List   func(query map[string]string, container *Context) (events.APIGatewayProxyResponse, error)
-	Add    func(body string, container *Context) (events.APIGatewayProxyResponse, error)
-	Get    func(params map[string]string, container *Context) (events.APIGatewayProxyResponse, error)
-	Edit   func(params map[string]string, body string, container *Context) (events.APIGatewayProxyResponse, error)
-	Delete func(params map[string]string, container *Context) (events.APIGatewayProxyResponse, error)
+	List   func(query map[string]string, ctx *Context) (events.APIGatewayProxyResponse, error)
+	Add    func(body string, ctx *Context) (events.APIGatewayProxyResponse, error)
+	Get    func(params map[string]string, ctx *Context) (events.APIGatewayProxyResponse, error)
+	Edit   func(params map[string]string, body string, ctx *Context) (events.APIGatewayProxyResponse, error)
+	Delete func(params map[string]string, ctx *Context) (events.APIGatewayProxyResponse, error)
 }
 
 // Dispatch dispatches request to matching handler
-func Dispatch(req events.APIGatewayProxyRequest, container *Context, handler HandlerSpec) (events.APIGatewayProxyResponse, error) {
+func Dispatch(req events.APIGatewayProxyRequest, ctx *Context, handler HandlerSpec) (events.APIGatewayProxyResponse, error) {
 	switch req.HTTPMethod {
 	case "GET":
 		if req.PathParameters != nil {
-			return handler.Get(req.PathParameters, container)
+			return handler.Get(req.PathParameters, ctx)
 		}
-		return handler.List(req.QueryStringParameters, container)
+		return handler.List(req.QueryStringParameters, ctx)
 	case "POST":
-		return handler.Add(req.Body, container)
+		return handler.Add(req.Body, ctx)
 	case "PUT":
-		return handler.Edit(req.PathParameters, req.Body, container)
+		return handler.Edit(req.PathParameters, req.Body, ctx)
 	case "DELETE":
-		return handler.Delete(req.PathParameters, container)
+		return handler.Delete(req.PathParameters, ctx)
 	}
 
 	return NewMessageError(400, "error.request.invalid")

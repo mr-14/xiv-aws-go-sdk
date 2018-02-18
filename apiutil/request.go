baab9d462512795539f99eb473af4269f4f7013c
zj <suke.shou@gmail.com>
2018-02-18 15:52:15 +0800

    Rearrange handler signatures

--- apiutil/request.go
+++ apiutil/request.go
@@ -1,13 +1,11 @@
 package apiutil
 
 import (
-	"context"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
-// Container defines service ctx
+// Container defines service container
 type Container struct {
 	Params   map[string]string
 	DynamoDB *dynamodb.DynamoDB
@@ -16,28 +14,27 @@ type Container struct {
 
 // HandlerSpec defines request handlers
 type HandlerSpec struct {
-	List    func(query map[string]string, ctx *Container) (events.APIGatewayProxyResponse, error)
-	Add     func(body string, ctx *Container) (events.APIGatewayProxyResponse, error)
-	Get     func(params map[string]string, ctx *Container) (events.APIGatewayProxyResponse, error)
-	Edit    func(params map[string]string, body string, ctx *Container) (events.APIGatewayProxyResponse, error)
-	Delete  func(params map[string]string, ctx *Container) (events.APIGatewayProxyResponse, error)
-	DBEvent func(ctx context.Context, e events.DynamoDBEvent)
+	List   func(container *Container, query map[string]string) (events.APIGatewayProxyResponse, error)
+	Add    func(container *Container, body string) (events.APIGatewayProxyResponse, error)
+	Get    func(container *Container, params map[string]string) (events.APIGatewayProxyResponse, error)
+	Edit   func(container *Container, params map[string]string, body string) (events.APIGatewayProxyResponse, error)
+	Delete func(container *Container, params map[string]string) (events.APIGatewayProxyResponse, error)
 }
 
 // Dispatch dispatches request to matching handler
-func Dispatch(req events.APIGatewayProxyRequest, ctx *Container, handler HandlerSpec) (events.APIGatewayProxyResponse, error) {
+func Dispatch(req events.APIGatewayProxyRequest, container *Container, handler HandlerSpec) (events.APIGatewayProxyResponse, error) {
 	switch req.HTTPMethod {
 	case "GET":
 		if req.PathParameters != nil {
-			return handler.Get(req.PathParameters, ctx)
+			return handler.Get(container, req.PathParameters)
 		}
-		return handler.List(req.QueryStringParameters, ctx)
+		return handler.List(container, req.QueryStringParameters)
 	case "POST":
-		return handler.Add(req.Body, ctx)
+		return handler.Add(container, req.Body)
 	case "PUT":
-		return handler.Edit(req.PathParameters, req.Body, ctx)
+		return handler.Edit(container, req.PathParameters, req.Body)
 	case "DELETE":
-		return handler.Delete(req.PathParameters, ctx)
+		return handler.Delete(container, req.PathParameters)
 	}
 
 	return NewMessageError(400, "error.request.invalid")
